e3569b61684c1999dc364ae0257af4317dab0550
Patrick Becker <patrickgbecker@gmail.com>
2022-04-05 18:12:27 -0500

    Initial Commit

---
+++ ingredientArrays.js
@@ -0,0 +1,47 @@
+var sides = [
+          'Bacon & Cheddar Grits',
+          'Dandelion Naan',
+          'Blue Cheese Deviled Eggs',
+          'Seared Cauliflower',
+          'Blistered Shishito Peppers',
+          'Massaman Curry Chicken Empanadas',
+          'Sweet Pea Saffron Rice',
+          'Fried 3 Sisters Cakes',
+          'Mini Wonton Quiche',
+          'Potatoes Au Gratin',
+          'Brussel Sprout Casserole',
+          'Lambas Bread',
+          'Tater Salad',
+          ];
+
+var mains = [
+          'Deconstructed Venison Egg Rolls',
+          'Slow-Cooked Achiote Carnitas Tacos',
+          'Seared Scallops w/ Brown Butter-Lemon Sauce',
+          'Coconut-Miso Salmon Curry',
+          'Okonomiyaki(Seafood Pancakes)',
+          'Blanquette de Veau w/ Escargots',
+          'Yakitori',
+          'Cioppino Stew',
+          'Roast Suckling Pig w/ Sardines',
+          'Anticucho de Corazon',
+          'Ceviche Mixto',
+          'Spaghetti alla Puttanesca',
+          'Porchetta Sandwich',
+          ];
+
+var desserts = [
+          'Mary Berry's Fraisier',
+          'Mary Berry's Chocolate Roulade',
+          'Mary Berry's Coffee & Walnut Battenburg',
+          'Paul Hollywood's Iced Fingers',
+          'Mary Berry's Tarte Au Citron',
+          'Victoria Sponge',
+          'Brioche Frangipane Apple Pudding',
+          'Apple & Lemon Sandwich Cake',
+          'Savarin with Chantilly Cream',
+          'Marjolaine',
+          'Dampfnudel',
+          'Viennese Whirls'
+          'Hazelnut Dacquoise',
+          ];
