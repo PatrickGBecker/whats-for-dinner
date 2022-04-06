fdc5318ecb89ab7bc69a6311c518a55ccef05033
Patrick Becker <patrickgbecker@gmail.com>
2022-04-06 17:13:21 -0500

    Add create random dish function

--- main.js
+++ main.js
@@ -6,15 +6,32 @@ var randomDish = document.querySelector('.random-dish');
 var letsCookButton = document.querySelector('.cook-button');
 var input = document.querySelectorAll(`input [name = 'course']`);
 
-
+//Randomizer:
 function random(array) {
     return Math.floor(Math.random() * array.length);
 };
 
-
+//Event listeners:
 clearButton.addEventListener('click', returnCookPot);
 
 letsCookButton.addEventListener('click', function() {
     event.preventDefault()
     makeRandomDish()
 });
+//Functions:
+functions createRandomDish() {
+        var course;
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].value === 'entire-meal') {
+              uncheckButton(input[i]);
+              entireMeal();
+              loadAnimation();
+          }  else {
+              loadAnimation();
+              uncheckButton(input[i]);
+              course = eval(input[i].value);
+              currentDish.innerHTML = course[random(course)];
+          }
+        }
+
+}
